afc8628d819e3fe0f2aeb4c8e0ab5a24c8706ece
Jason J. Gullickson <mr@jasongullickson.com>
2020-09-12 14:19:11 -0500

    Initial experiments with direct framebuffer graphics.

---
+++ experiments/fbgraphics.c
@@ -0,0 +1,53 @@
+#include <linux/fb.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+
+uint32_t pixel_color(uint8_t r, uint8_t g, uint8_t b, struct fb_var_screeninfo *vinfo)
+{
+  return (r<<vinfo->red.offset) | (g<<vinfo->green.offset) | (b<<vinfo->blue.offset);
+}
+
+int main()
+{
+  struct fb_fix_screeninfo finfo;
+  struct fb_var_screeninfo vinfo;
+
+  int fb_fd = open("/dev/fb0",O_RDWR);
+
+  //Get variable screen information
+  ioctl(fb_fd, FBIOGET_VSCREENINFO, &vinfo);
+  vinfo.grayscale=0;
+  vinfo.bits_per_pixel=32;
+  ioctl(fb_fd, FBIOPUT_VSCREENINFO, &vinfo);
+
+  ioctl(fb_fd, FBIOGET_VSCREENINFO, &vinfo);
+
+  ioctl(fb_fd, FBIOGET_FSCREENINFO, &finfo);
+
+  long screensize = vinfo.yres_virtual * finfo.line_length;
+
+  uint8_t *fbp = mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, (off_t)0);
+
+  int x,y;
+
+  printf("vinfo.xres = %d\n", vinfo.xres);
+  printf("vinfo.yres = %d\n", vinfo.yres);
+
+  for (x=0;x<vinfo.xres;x++)
+  {
+    
+    for (y=0;y<vinfo.yres;y++)
+    {
+      
+      long location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) + (y+vinfo.yoffset) * finfo.line_length;
+      *((uint32_t*)(fbp + location)) = pixel_color(0xFF,0x00,0xFF, &vinfo);
+      
+    }
+    
+  }
+
+  return 0;
+}
---
+++ experiments/notes.md
@@ -0,0 +1,5 @@
+When running code that talks to the framebuffer directly (fb dev), you need to use `sudo`.  Programs will run regardless, but they can't determine the framebuffer parameters and will crash accordingly.
+
+`fbset` is  a useful command when working with the framebuffer
+
+
