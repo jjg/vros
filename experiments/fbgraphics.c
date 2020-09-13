8f905e5db97028af003030bddf99edc43fb96f3b
Jason J. Gullickson <mr@jasongullickson.com>
2020-09-13 07:46:05 -0500

    WIP double buffering.

--- experiments/fbgraphics.c
+++ experiments/fbgraphics.c
@@ -13,7 +13,10 @@ struct fb_fix_screeninfo finfo;
 struct fb_var_screeninfo vinfo;
 int fb_fd;
 long screensize;
-uint8_t *fbp;
+//uint8_t *fbp;
+
+uint8_t *fbp;   //Front buffer base pointer
+uint8_t *bbp;   //back buffer base pointer
 
 void fb_init() 
 {
@@ -31,7 +34,10 @@ void fb_init()
 
     screensize = vinfo.yres_virtual * finfo.line_length;
 
-    fbp = mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, (off_t)0);
+    //fbp = mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, (off_t)0);
+
+    fbp = mmap(0, screensize*2, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, (off_t)0);
+    bbp = fbp + screensize;
 
     // debug
     printf("vinfo.xres = %d\n", vinfo.xres);
@@ -41,6 +47,36 @@ void fb_init()
 uint32_t pixel_color(uint8_t r, uint8_t g, uint8_t b)
 {
     return (r << vinfo.red.offset) | (g << vinfo.green.offset) | (b << vinfo.blue.offset);
+}
+
+void clear()
+{
+    int x,y;
+
+    for (x=0;x<vinfo.xres;x++)
+        for (y=0;y<vinfo.yres;y++)
+        {
+            long location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) + (y+vinfo.yoffset) * finfo.line_length;
+            *((uint32_t*)(bbp + location)) = pixel_color(0xFF,0x00,0xFF);
+        }
+}
+
+// experimental double buffering
+void swap_buffers()
+{
+    if (vinfo.yoffset==0)
+            vinfo.yoffset = screensize;
+    else
+            vinfo.yoffset=0;
+
+    //"Pan" to the back buffer
+    ioctl(fb_fd, FBIOPAN_DISPLAY, &vinfo);
+
+    //Update the pointer to the back buffer so we don't draw on the front buffer
+    long tmp;
+    tmp=fbp;
+    fbp=bbp;
+    bbp=tmp;
 }
 
 void draw(int x, int y, int pixel)
@@ -138,9 +174,10 @@ int main()
 
     fb_init();
 
-    //draw(10, 10, pixel_color(0xFF, 0xFF, 0xFF));
-    //draw_line(10,10,100,100,pixel_color(0xFF,0xFF,0xFF));a
-    draw_circle(100,100,50,pixel_color(0,255,0));
+    draw(10, 10, pixel_color(0xFF, 0xFF, 0xFF));
+    //draw_line(10,10,100,100,pixel_color(0xFF,0xFF,0xFF));
+    //draw_circle(100,100,50,pixel_color(0,255,0));
+    //swap_buffers();
 
     return 0;
 }
